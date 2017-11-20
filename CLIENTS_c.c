a83e0f62c59207000ef036c54b9d7ca03699c720
jakemckenzie <jake314@uw.edu>
2017-11-19 23:36:21 -0800

    Add files via upload

--- CLIENTS_c.c
+++ CLIENTS_c.c
@@ -1,69 +1,169 @@
 #include "CLIENTS_h.h"
 
 Client createClient(FILE *fptr) {
-    Client client = malloc(sizeof(struct client_type));
-    char tempstr[50];
-    client->id = malloc(sizeof(char) * 100);
-    fgets(tempstr, 100, fptr);
-    tempstr[strlen(tempstr + 2)] = '\0';
-    strcpy(client->id, tempstr);
+
+  Client client = malloc(sizeof(struct client_type));
+
+  char tempstr[50];
+
+  client->id = malloc(sizeof(char) * 100);
+
+  fgets(tempstr, 100, fptr);
+
+  tempstr[strlen(tempstr + 2)] = '\0';
+
+  strcpy(client->id, tempstr);
     
-    client->name = malloc(sizeof(char) * 100);
-    fgets(tempstr, 100, fptr);
-    tempstr[strlen(tempstr + 2)] = '\0';
-    strcpy(client->name, tempstr);
+  client->name = malloc(sizeof(char) * 100);
+
+  fgets(tempstr, 100, fptr);
+
+  tempstr[strlen(tempstr + 2)] = '\0';
+
+  strcpy(client->name, tempstr);
     
-    client->phonenumber = malloc(sizeof(char) * 100);
-    fgets(tempstr, 100, fptr);
-    tempstr[strlen(tempstr + 2)] = '\0';
-    strcpy(client->phonenumber, tempstr);
+  client->phonenumber = malloc(sizeof(char) * 100);
+
+  fgets(tempstr, 100, fptr);
+
+  tempstr[strlen(tempstr + 2)] = '\0';
+
+  strcpy(client->phonenumber, tempstr);
     
-    client->email = malloc(sizeof(char) * 100);
-    fgets(tempstr, 100, fptr);
-    tempstr[strlen(tempstr + 2)] = '\0';
-    strcpy(client->email, tempstr);
+  client->email = malloc(sizeof(char) * 100);
+
+  fgets(tempstr, 100, fptr);
+
+  tempstr[strlen(tempstr + 2)] = '\0';
+
+  strcpy(client->email, tempstr);
     
   return client;
+
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This destroys the client                                                          ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
 void destroyClient(Client client) {
+
   free(client->id);
+
   free(client->name);
+
   free(client->phonenumber);
+
   free(client->email);
-//  free(client);
+
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This prints the client                                                            ///
+///////////////////////////////////////////////////////////////////////////////////////////
 
 void fprintClient(FILE *fptr, Client client) {
+
   fprintf(fptr,"%s,%s,,%s,%s\n,", client->id, client->name, client->phonenumber, client->email);  
-}
-char* getID(Client client) {
-  return client->id;
-}
-char* getName(Client client) {
-  return client->name;
-}
-char* getNumber(Client client) {
-  return client->phonenumber;
-}
-char* getEmail(Client client) {
-  return client->email;
-}
-void setID(Client client, char* newid) {
-  strcpy(client->id, newid);  
-}
-void setName(Client client, char* newname) {
-  strcpy(client->name, newname);
-}
-void setNumber(Client client, char* newnumber) {
-  strcpy(client->phonenumber, newnumber); 
-}
-void setEmail(Client client, char* newemail) {
-  strcpy(client->email, newemail);
-}
-int compareID(Client client1, Client client2) {
-  return strcmp(client1->id,client2->id);
+
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This gets the client ID                                                           ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
+char* getID(Client client) {
+
+  return client->id;
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This gets the client name                                                         ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
+char* getName(Client client) {
+
+  return client->name;
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This gets the client number                                                       ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
+char* getNumber(Client client) {
+
+  return client->phonenumber;
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This gets the client email                                                        ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
+char* getEmail(Client client) {
+
+  return client->email;
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This sets the client ID                                                           ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
+void setID(Client client, char* newid) {
+
+  strcpy(client->id, newid);  
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This sets the client name                                                         ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
+void setName(Client client, char* newname) {
+
+  strcpy(client->name, newname);
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This sets the client number                                                       ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
+void setNumber(Client client, char* newnumber) {
+
+  strcpy(client->phonenumber, newnumber); 
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This sets the client email                                                        ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
+void setEmail(Client client, char* newemail) {
+
+  strcpy(client->email, newemail);
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This compares the client ID                                                       ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
+int compareID(Client client1, Client client2) {
+
+  return strcmp(client1->id,client2->id);
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This prints the client                                                            ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
 void printclient(Client client) {
+
   printf("%s\n%s\n%s\n%s\n",client->id, client->name, client->phonenumber, client->email);
+
 }
--- driver.c
+++ driver.c
@@ -1,125 +1,219 @@
 #include "CLIENTS_h.h"
 #include "listADTgen.h"
 #include "STOCKS_h.h"
+///////////////////////////////////////////////////////////////////////////////////////////
+///                  __________                   __               __                   ///
+///                  \______   \_______  ____    |__| ____   _____/  |_                 ///
+///                   |     ___/\_  __ \/  _ \   |  |/ __ \_/ ___\   __\                ///
+///                   |    |     |  | \(  <_> )  |  \  ___/\  \___|  |                  ///
+///                   |____|     |__|   \____/\__|  |\___  >\___  >__|                  ///
+///                                          \______|    \/     \/                      ///
+///                            ___________                                              ///
+///                            \_   _____/___  __ _________                             ///
+///                             |    __)/  _ \|  |  \_  __ \                            ///
+///                             |     \(  <_> )  |  /|  | \/                            ///
+///                             \___  / \____/|____/ |__|                               ///   
+///                                 \/                                                  ///
+///////////////////////////////////////////////////////////////////////////////////////////
 
-int main() {
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This assignment was done by Timothy Olchondra and Jake McKenzie. Tim ran          ///
+///   it on css.gate server under gcc -std=gnu90 -o pr4 pr4.c and Jake ran              ///
+///   the file in Ubuntu 16.4 under gcc -std=gnu90 -o pr4 pr4.c . In our assignment     ///
+///   we have one set of abstract data types that follow the principle of               ///
+///   data hiding along with an ADT that follows the same principles.Specs said         ///
+///   that it was allowed but not required to follow data hiding principles but         ///
+///   we assume it was good practice to do so so we did it that way.                    ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+int main(void) {
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   Here we take in all the files and open the summary file with which we will        ///
+///   write to. The part written below was written by Tim. We are assuming correct      ///
+///   inputs but if there is nothing in the file a warning is sent. - Jake              ///
+///////////////////////////////////////////////////////////////////////////////////////////  
+
   FILE *clientfptr = fopen("clients.txt","r");
   FILE *stockfptr = fopen("stocks.csv", "r");
   FILE *stockclientfptr = fopen("stock_client.txt","r");
   FILE *summary = fopen("summary.csv", "w");
   
   if(clientfptr == NULL) {
+
     printf("Could not find client file");
+
     return -1;
+
   }
+
   if(stockfptr == NULL) {
+
+
     printf("Could not find stock file");
+
     return -1;
+
   }
+
   if(stockclientfptr == NULL) {
+
     printf("Could not find stock_client file");
+
     return -1;
+
   } 
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   Here is where we create the two generic lists for the clients and stocks.         ///
+///////////////////////////////////////////////////////////////////////////////////////////  
   
   ListType listClients = create(sizeof(struct client_type),compareID);
   ListType listStocks = create(sizeof(struct stocks_type),compareSymbol);
   
   
- // printf("%d", sizeof(struct client_type));
   char buffer[100];
-  int countline = 0;
-  int i,j;
+
+  int countline = 0, i, j;
   
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This is an important step. To enable manpulation of the data structure we         ///
+///   needed a way with which to count the lines. Knowing the lines allowed us          ///
+///   to take full advantage of the push function provided for us with the assignement. ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
   while(fgets(buffer, 100, clientfptr) != NULL) {
+
     countline++;
+
   }
- // printf("%d\n", countline);
+
   fclose(clientfptr);
+
   clientfptr = fopen("clients.txt","r");
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   Here we take advantage of the push function given to us in files.                 ///
+///////////////////////////////////////////////////////////////////////////////////////////
+  
   for(i = 0; i < countline/4; i++) {
+
     push(listClients, createClient(clientfptr));
+
   }
   
   countline = 0;
+  
+///////////////////////////////////////////////////////////////////////////////////////////
+///   Like above but now with stocks we read the lines inorder to use the push function ///
+///   provided                                                                          ///
+///////////////////////////////////////////////////////////////////////////////////////////
+  
   while(fgets(buffer, 100, stockfptr) != NULL) {
+
     countline++;
+
   }
-//  printf("%d\n", countline);
+
   fclose(stockfptr);
+
   stockfptr = fopen("stocks.csv","r");
-  //fscanf(stockfptr,"%[^\n]", buffer);
+
   fgets(buffer, 100, stockfptr);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   Here we take advantage of the push function given to us in files.                 ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
   for(i = 0; i < countline-1; i++) {
+
     push(listStocks, createStock(stockfptr));
+
   }
   
   
   printf("%d\n", size_is(listClients));
+
   printf("%d\n", size_is(listStocks));
+
+  printl(listStocks, printStock);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///   This is where the file is written to. It takes the information gathered above     ///
+///   then writes it to summary. This is accomplished by keeping track of our position  ///
+///   within the file and our total position.                                           ///
+///////////////////////////////////////////////////////////////////////////////////////////
+
   
-  
-//  int position = 3;
-  //printf("%s\n", getCharl(listClients, getName, position));
-  //printf("%.2lf\n", getDoublel(listStocks, getPrice, position));
-  //printl(listClients, printclient);
-    printl(listStocks, printStock);
-  
-  
-  char idmatch[100];
-  int stockcount;
-  char stockmatch[100];
-  int numshares;
+  char idmatch[100], stockmatch[100];
+
+  int stockcount, numshares;
   
   for(i = 0; i < size_is(listClients); i++) {
+
     double total = 0;
+
     int position = 0;
+
     fscanf(stockclientfptr,"%s %d", idmatch, &stockcount); 
+
     printf("%s\n", idmatch);
+
     printf("%d\n", stockcount);
-  //search for client with same id 
+
     position = matchElementString(listClients, getID, idmatch);
-  //print info
+
     fprintElement(listClients, summary, fprintClient, position); 
     
     for(j = 0; j < stockcount; j++) {
-    //search for stock to get price of stock
+
       fscanf(stockclientfptr, "%s %d", stockmatch, &numshares);
+
       fprintf(summary,"%s,%d,", stockmatch, numshares); 
+
       position = matchElementString(listStocks, getSymbol, stockmatch);
+
       fprintElement(listStocks, summary, fprintStockPrice, position);
-      //multiply numshares with price of stock
-      //add to total
+
       total += numshares * getDoublel(listStocks, getPrice, position);   
     } 
+
     fprintf(summary, "%.2lf,,,\n", total);
+
   }
   
  
- 
- 
+/////////////////////////////////////////////////////////////////////////////////////////// 
+///  This destroys all the elements in the clients list.                                /// 
+///////////////////////////////////////////////////////////////////////////////////////////
   
   for(i = 0; i < size_is(listClients); i++) {
+
     destroyElement(listClients, destroyClient, i);
 
   }
- // printf("%d\n", size_is(listClients));
+
+/////////////////////////////////////////////////////////////////////////////////////////// 
+///  This destroys all the elements in the stocks list.                                 /// 
+///////////////////////////////////////////////////////////////////////////////////////////
+
+  
   for(i = 0; i < size_is(listStocks); i++) {
+
     destroyElement(listStocks, destroyStock, i);
 
   }
   
- // printf("%d\n", size_is(listStocks));
- 
-  //make_empty(listClients);
-//  printf("%d\n", size_is(listClients));
-//  make_empty(listStocks);
-  // printf("%d\n", size_is(listStocks));
+
   destroy(listClients);
   destroy(listStocks);
  
   
   fclose(clientfptr);
   fclose(stockfptr);
+
   return 0;
     
 }
